e4522ca66c65b0ac48e424859799bfc9c8f41b95
H-M-Masud <hmmasud163@gmail.com>
2022-05-01 05:42:01 +0600

    added onAuth state changed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase()
     return (
         <div>
-            <h2>Please Log In!!!</h2>
+            <h3>Please Login</h3>
+            <div style={{margin:'20px'}}>
+                <button onClick={signInWithGoogle}>Google sign In</button>
+            </div>
+            <form>
+                <input type="email" placeholder='your email' />
+                <br />
+                <input type="password" name="" placeholder='password' id="" />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
