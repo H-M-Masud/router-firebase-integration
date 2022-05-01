898e392e6aa65951ecc6dd1d67c3fce21f14d817
H-M-Masud <hmmasud163@gmail.com>
2022-05-01 12:58:13 +0600

    added requredauth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,16 +2,28 @@ import React from 'react';
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
 import {getAuth} from 'firebase/auth';
 import app from '../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const auth = getAuth(app);
 
 const Login = () => {
-    const [signInWithGoogle] = useSignInWithGoogle(auth)
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location?.state?.from?.pathname || '/';
+
+    const handleGoogleSignIn = () =>{
+        signInWithGoogle()
+        .then(() =>{
+            navigate(from, {replace: true})
+        })
+    }
     return (
         <div>
             <h3>Please Login</h3>
             <div style={{margin:'20px'}}>
-                <button onClick={() =>signInWithGoogle()}>Google sign In</button>
+                <button onClick={handleGoogleSignIn}>Google sign In</button>
             </div>
             <form>
                 <input type="email" placeholder='your email' />
